52fcc0632521dc4d604d4e2117a879adef2a45d3
damienien <d.choquet@hotmail.com>
2023-08-21 08:38:40 -0400

    clean previous commit

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,6 @@ def results(request, question_id):
     context = {
 		'question': question,
 		'question_id': question_id,
-  #toto
 	}
     return render(request, "polls/results.html", context)
 
